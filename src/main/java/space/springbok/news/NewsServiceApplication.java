42b0a32ad20cab1f134e73d7e5ea19b1544239ca
John Spangenberg <dev@springbok.space>
2019-05-30 16:36:03 +0200

    Removal of mongo db dependencies

--- src/main/java/space/springbok/news/NewsServiceApplication.java
+++ src/main/java/space/springbok/news/NewsServiceApplication.java
@@ -1,18 +1,30 @@
 package space.springbok.news;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  */
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class NewsServiceApplication {
+
+    @Value("${service.instance.name}")
+    private String instance;
 
     public static void main(String[] args) {
         SpringApplication.run(NewsServiceApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String message() {
+        return "Hello from instance " + instance;
     }
 
 }
