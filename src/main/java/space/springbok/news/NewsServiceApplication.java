93a3dcc6ce1415907516fb492ce4deb703c7124e
John Spangenberg <dev@springbok.space>
2019-06-14 17:55:24 +0200

    Experiment with Zuul Filters.

--- src/main/java/space/springbok/news/NewsServiceApplication.java
+++ src/main/java/space/springbok/news/NewsServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +35,11 @@ public class NewsServiceApplication {
     @RequestMapping("/")
     public String message() {
         return "Hello from instance " + instance;
+    }
+
+    @RequestMapping("/fromLocation")
+    public String fromLocation(@RequestHeader("x-location") String location) {
+        return "Hello from location: " + location;
     }
 
     @RequestMapping("/printConfig")
